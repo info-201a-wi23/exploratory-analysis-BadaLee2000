06c1c369059821e2bce538aa9e0e983086e5152f
Tamia Ouch <Tamiaouch@icloud.com>
2023-02-16 12:05:59 -0800

    Tamia and Jason's Portion

--- Chart2.R
+++ Chart2.R
@@ -10,4 +10,6 @@ mentalhealth_df <- read.csv("https://raw.githubusercontent.com/info-201a-wi23/ex
 age_df <- mentalhealth_df %>% group_by(age) %>% select(depression_severity)
 #creates bar chart, uses color for depression severity
 ggplot(data = age_df) +
-  geom_histogram(aes(x = age, fill = depression_severity)) 
+  geom_histogram(aes(x = age, fill = depression_severity)) +
+  labs(title = "Severity of Depression in College Students", x = "Age", y = "Student Count", fill = "Depression Severity")
+
