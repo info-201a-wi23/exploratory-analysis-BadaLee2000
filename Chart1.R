4803ab3864ccffa353d1406f7c6e9c97cb9ebafb
Tamia Ouch <Tamiaouch@icloud.com>
2023-02-16 12:05:37 -0800

    Tamia's Portion

--- Chart1.R
+++ Chart1.R
@@ -11,7 +11,10 @@ mentalhealth_df <- read.csv("https://raw.githubusercontent.com/info-201a-wi23/ex
 gender_df <- mentalhealth_df %>% group_by(gender) %>% select(depression_severity)
 #creates bar chart, uses color for gender
 ggplot(data = gender_df) +
-  geom_bar(aes(x = depression_severity, fill = gender)) 
+  geom_bar(aes(x = depression_severity, fill = gender)) +
+  labs(title = "Severity of Depression in College Students", x = "Depression Severity", y = "Student Count", fill = "Gender")
+
+
 
 
 
