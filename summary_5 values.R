e2a26d8ad099ea41311eb2ba7eb64b780faeb552
jamiehankim <jamiek00@uw.edu>
2023-02-16 21:16:13 -0800

    edits on the five value for Jamie

--- summary_5 values.R
+++ summary_5 values.R
@@ -1,13 +1,15 @@
 # The Five Values
 summary <- read.csv("depression_anxiety_data.csv", stringsAsFactors = FALSE)
 
-# Which gender felt more anxiety? FEMALE 
+# Which gender felt more anxiety? 
+# FEMALE 
 depression_gender_female <- summary_data %>% filter(gender == "female" & age > 17 & age < 25) %>% filter(anxiousness == "TRUE")
 nrow(depression_gender_female)
 depression_gender_male <- summary_data %>% filter(gender == "male" & age > 17 & age < 25) %>% filter(anxiousness == "TRUE")
 nrow(depression_gender_male)
 
-# In which age group did the most suicide happen? 10, 27, 12, 7, 10, 1, 1
+# In which age group did the most suicide happen? 
+# 10, 27, 12, 7, 10, 1, 1
 depression_suicidal_18 <- summary_data %>% filter(age == 18) %>% filter(suicidal == "TRUE")
 nrow(depression_suicidal_18)
 
@@ -29,15 +31,18 @@ nrow(depression_suicidal_23)
 depression_suicidal_24 <- summary_data %>% filter(age == 24) %>% filter(suicidal == "TRUE")
 nrow(depression_suicidal_24)
 
-# find the number of students who experienced depression (209)
+# find the number of students who experienced depression 
+# 209
 depression_depressiveness <- summary_data %>% filter(17 < age & age < 25) %>% filter(depressiveness == "TRUE")
 nrow(depression_depressiveness)
 
-# find the number of students who have been diagnosed depression (65)
+# find the number of students who have been diagnosed depression 
+# 65
 depression_diagnosis <- summary_data %>% filter(17 < age & age < 25) %>% filter(depression_diagnosis == "TRUE")
 nrow(depression_diagnosis)
 
-# the number of students who committed suicide (68) 
+# the number of students who committed suicide 
+# 68
 depress_suicidal <- summary_data %>% filter(17 < age & age < 25) %>% filter(suicidal == "TRUE")
 nrow(depress_suicidal)
 
