8a2019dd5f06959cc66f3a8f53922983a9a3d4c5
jasoncabusao <jasoncabusao@Jasons-MacBook-Pro-15.local>
2023-02-18 21:43:13 -0800

    Jason: Fixed chart i and iii

--- Chart1.R
+++ Chart1.R
@@ -1,15 +1,24 @@
+# Load the necessary packages
 library(dplyr)
 library(ggplot2)
-#creates data frame with information from csv file
+
+# Creates a data frame with information from csv file
 mentalhealth_df <- read.csv("https://raw.githubusercontent.com/info-201a-wi23/exploratory-analysis-BadaLee2000/main/depression_anxiety_data.csv")
-#creates dataframe with just gender and depression severity
-gender_df <- mentalhealth_df %>% group_by(gender) %>% select(depression_severity)
-#creates bar chart, uses color for gender
-ggplot(data = gender_df) +
-  geom_bar(aes(x = depression_severity, fill = gender)) +
-  labs(title = "Severity of Depression in College Students", x = "Depression Severity", y = "Student Count", fill = "Gender")
 
+# Creates a dataframe with just gender and depression severity
+gender_df <- mentalhealth_df %>% 
+  group_by(gender) %>% 
+  select(depression_severity)
 
-
-
-
+# Creates a bar chart and uses color for gender
+ggplot(data = mentalhealth_df) +
+  geom_bar(aes(x = depression_severity,
+               fill = gender),
+           position = "dodge") +
+  scale_x_discrete(limits = c("none", "None-minimal", "Mild", "Moderate", "Moderately severe", "Severe")) +
+  theme(axis.text = element_text(size = 8)) +
+  scale_fill_manual(values = c("#DE3163","#0096FF")) +
+    labs(title = "Severity of Depression in College Students",
+         x = "Depression Severity", 
+         y = "Count of College Students",
+         fill = "Gender")
--- Chart3.R
+++ Chart3.R
@@ -1,4 +1,3 @@
-
 # Load the necessary packages
 library(dplyr)
 library(ggplot2)
@@ -7,12 +6,14 @@ library(ggplot2)
 mental_health_df <- read.csv("https://raw.githubusercontent.com/info-201a-wi23/exploratory-analysis-BadaLee2000/main/depression_anxiety_data.csv", stringsAsFactors = FALSE)
 
 # Create a horizontal bar chart showing the relationship between a the severity of a college students depression and the year of college they're in.
-
-ggplot(mental_health_df,
-       aes(x = school_year,
-           y = 1.0,
-           fill = depression_severity)) +
-  geom_bar(position = "fill", stat = "identity") +
-  coord_flip() + 
-  labs(title = "Severity of Depression in College Students", x = "Count of College Students", y = "Year of College", fill = "Depression Severity")
-
+ggplot(data = mentalhealth_df) +
+  geom_col(aes(x = school_year,
+             y = frequency(depression_severity),
+             fill = depression_severity),
+         position = "fill") +
+  scale_fill_brewer(palette = "Oranges") +
+  coord_flip() +
+  labs(title = "Severity of Depression in College Students",
+       x = "Year of College",
+       y = "Count of College Students",
+       fill = "Depression Severity")
